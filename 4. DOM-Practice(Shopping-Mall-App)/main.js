94c9cd89fb6b1a4870155d00fa98cd1f7edf20dd
BVBFD <lsevina126@gmail.com>
2021-08-25 21:39:00 +0900

    4. DOM Practice Shopping mall Code, 5. About Event of JavaScript
    4. Shopping Mall List Code (using InnerHTML and dataset)
    
    5. *capturing and bubbling
       *preventDefault(): passive, active
       *delegation

--- 4. DOM-Practice(Shopping-Mall-App)/main.js
+++ 4. DOM-Practice(Shopping-Mall-App)/main.js
@@ -7,42 +7,55 @@ const delBtn = document.querySelector('.item__delete');
 // 2. 플러스 버튼 누르면 추가되는 단계로 넘어가고
 // 3. DOM 요소를 만들고 조합하고 추가
 
+let id = 0;
 function onAdd(){
     const text = input.value;
     if(text === ''){
         return;
     }
-    const item = createItem(text);
     
     const item__row = document.createElement('li');
     item__row.setAttribute('class', 'item__row');
-    item__row.appendChild(item);
-    const item__divider = document.createElement('div');
-    item__divider.setAttribute('class', 'item__divider');
-    item__row.appendChild(item__divider);
+    item__row.setAttribute('data-id', id);
+    item__row.innerHTML = `
+    <div class="item">
+        <span class="item__name">${text}</span>
+        <button class="item__delete" data-id=${id}>
+            <i class="fas fa-trash-alt" data-id=${id}></i>
+        </button>
+    </div>
+    <div class="item__divider"></div>
+    `;
     items.appendChild(item__row);
+    // const item = createItem(text);
+    // item__row.appendChild(item);
+    // const item__divider = document.createElement('div');
+    // item__divider.setAttribute('class', 'item__divider');
+    // item__row.appendChild(item__divider);
+    // items.appendChild(item__row);
     input.value = '';
     input.focus();
     item__row.scrollIntoView();
+    id++;
 };
 
-function createItem(text){
-    const item = document.createElement('div');
-    item.setAttribute('class', 'item');
-    const item__name = document.createElement('span');
-    item__name.setAttribute('class', 'item__name');
-    item__name.innerText = text;
-    const item__delete = document.createElement('button');
-    item__delete.setAttribute('class', 'item__delete');
-    item__delete.innerHTML = `<i class="fas fa-trash-alt"></i>`;
-    item__delete.addEventListener('click', () => {
-        items.removeChild(item.parentNode);
-    });
+// function createItem(text){
+//     const item = document.createElement('div');
+//     item.setAttribute('class', 'item');
+//     const item__name = document.createElement('span');
+//     item__name.setAttribute('class', 'item__name');
+//     item__name.innerText = text;
+//     const item__delete = document.createElement('button');
+//     item__delete.setAttribute('class', 'item__delete');
+//     item__delete.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+//     item__delete.addEventListener('click', () => {
+//         items.removeChild(item.parentNode);
+//     });
 
-    item.appendChild(item__name);
-    item.appendChild(item__delete);
-    return item;
-};
+//     item.appendChild(item__name);
+//     item.appendChild(item__delete);
+//     return item;
+// };
 
 addBtn.addEventListener('click', () => {
     onAdd();
@@ -54,6 +67,10 @@ input.addEventListener('keypress', (event) => {
     }
 });
 
-delBtn.addEventListener('click', (event) => {
-    items.removeChild(event.target.parentNode.parentNode.parentNode);
+items.addEventListener('click', (event) => {
+    const id = event.target.dataset.id
+    if(id){
+        const deletedList = document.querySelector(`.item__row[data-id='${id}']`);
+        deletedList.remove();
+    }
 });
